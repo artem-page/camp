a66e47e51051d7d4a861b305216a103c46514a00
Art <you@example.com>
2023-12-14 13:47:05 -0500

    misc

--- index.js
+++ index.js
@@ -16,11 +16,14 @@ app.use(cors({optionsSuccessStatus: 200}));
 const api = require('./api/myAPI');
 app.use(api);
 
+
 let sslServer = https.createServer({
     key: fs.readFileSync('/etc/letsencrypt/live/camp.r1a1.xyz/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/camp.r1a1.xyz/cert.pem'),
     ca: fs.readFileSync('/etc/letsencrypt/live/camp.r1a1.xyz/fullchain.pem')
 }, app);
+
+app.use(vhost("camp.r1a1.xyz", app));
  
 const { port } = require('./config');
   
