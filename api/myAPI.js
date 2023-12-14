2dc25e43a15270a5dd91233399459a26fa831c9c
Art <you@example.com>
2023-12-14 14:33:34 -0500

    misc

--- api/myAPI.js
+++ api/myAPI.js
@@ -15,9 +15,24 @@ api.get("/", (req, res) => {
 api.get("/api/:date", (req, res) => {
 
     let dateReq = isNaN(req.params.date) ? req.params.date : parseInt(req.params.date)
-    let dateObj = new Date(dateReq)
+    let dateObj
+
+    /* 
+    The input arrives in the form of a string. While the new Date() function adeptly transforms it into a date, 
+    it encounters difficulty when the string resembles a numeric value or Unix timestamp. 
+    */
     
-    res.json({unix: dateObj.getTime(), utc: dateObj.toUTCString()})
+    if(isNaN(dateReq)) {
+        dateObj = new Date()
+        res.json({ unix: dateObj.getTime(), utc: dateObj.toUTCString() })
+    } else {
+        dateObj = new Date(dateReq)
+        if(dateObj.toUTCString() === "Invalid Date") {
+            res.json({ error : "Invalid Date" })
+        } else {
+            res.json({ unix: dateObj.getTime(), utc: dateObj.toUTCString() })
+        }
+    }
   
 })
 
