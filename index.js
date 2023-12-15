4dafc69345430c02cc784b2c9f630fa20295d90b
Art <rumiantsev.ca@gmail.com>
2023-12-15 13:44:43 -0500

    misc

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 
 // Mount static assets
 
-app.use("/assets", express.static(path.join(__dirname, "public")))
+app.use("/public", express.static(path.join(__dirname, "public")))
 
 // Define the home page route
 
