83207b1603ab41c00f5afba9da216116a25ad534
Art <you@example.com>
2023-12-15 09:30:00 -0500

    misc

--- api/myAPI.js
+++ api/myAPI.js
@@ -42,12 +42,13 @@ api.get("/api/timestamp-microservice/:date?", (req, res) => {
 
 api.get("/api/whoami", (req, res) => {
     let clientHeaders = req.headers
+    let clientIP = req.socket.remoteAddress
 
     /*
         An array available: host, connection, user-agent, cache-control, accept, accept-encoding, accept-language, etc.
     */
 
-    res.json({ host: clientHeaders["host"], language: clientHeaders["accept-language"], software: clientHeaders["user-agent"] })
+    res.json({ ipaddress: clientIP, language: clientHeaders["accept-language"], software: clientHeaders["user-agent"] })
 })
 
 module.exports = api
