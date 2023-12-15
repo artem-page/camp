26bd8f64698299f79d4e0652410fb455a5e4e174
Art <rumiantsev.ca@gmail.com>
2023-12-15 10:38:57 -0500

    misc

--- api/myAPI.js
+++ api/myAPI.js
@@ -41,8 +41,10 @@ api.get("/api/request-header-parser/whoami", (req, res) => {
     res.json({ ipaddress: clientIP, language: clientHeaders["accept-language"], software: clientHeaders["user-agent"] })
 })
 
-api.post("/api/shorturl", (req, res) => {
-    res.json({ original_url : req.body.original_url, short_url : 1 })
+api.route('/api/shorturl').get((req, res) => {
+    res.json({ original_url: req.query.original_url })
+}).post((req, res) => {
+    res.json({ original_url: req.body.original_url })
 })
 
 module.exports = api
