f948dba6b15ee8b519bb5ece04429747be6f3421
Art <rumiantsev.ca@gmail.com>
2023-12-16 15:08:27 -0500

    misc

--- api/myRouter.js
+++ api/myRouter.js
@@ -61,9 +61,8 @@ const Link = mongoose.model('link', {})
 
 apiRouter.post('/api/shorturl', (req, res) => {
 
-    let ipAddress
-    dns.lookup(req.body.original_url, (err, address, family) => {
-        ipAddress = address
+    let ipAddress = dns.lookup(req.body.original_url, (err, address, family) => {
+        return address
     })
 
     /*
