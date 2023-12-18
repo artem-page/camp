da8034b66fdb38bc660e326bfa62c1bb7a246d10
Art <rumiantsev.ca@gmail.com>
2023-12-18 14:12:24 -0500

    misc

--- api/myRouter.js
+++ api/myRouter.js
@@ -93,7 +93,7 @@ let Link = mongoose.model('links', linkSchema)
 
 apiRouter.post('/api/shorturl', (req, res) => {
 
-    if (isValidUrl(req.body.original_url)) {
+    if ( req.body.original_url ) {
 
         let originalUrl = removeProtocol(req.body.original_url)
 
